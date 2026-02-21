9bc652795599e158862b1ef151d349fbcb4a1fea
paraspingale-hub <paraspingales@gmail.com>
2026-02-21 14:28:51 +0530

    Added a new Python script for checking Pandas

---
+++ Demonstration_programs/Iteration_for.py
@@ -0,0 +1,11 @@
+#sequence programming
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+print("-"*9)
+
+#iteration programming
+for i in range(4): 
+    print("Jay Ganesh..")
+
